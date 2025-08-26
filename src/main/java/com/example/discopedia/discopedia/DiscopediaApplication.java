012b57f24c098501830c9ca7b5e5ff0e279a7f23
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-08-26 09:32:10 +0200

    build: add Spring Boot Initializr

---
+++ src/main/java/com/example/discopedia/discopedia/DiscopediaApplication.java
@@ -0,0 +1,13 @@
+package com.example.discopedia.discopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscopediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscopediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/discopedia/discopedia/DiscopediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discopedia.discopedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscopediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
