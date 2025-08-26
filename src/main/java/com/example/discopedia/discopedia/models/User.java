668750b196d7e9a2ee3e457c2c6fa5a90fb56d7a
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-08-26 09:50:07 +0200

    feat(User): add User Entity and fields (id, username, email, password) to User Entity

---
+++ src/main/java/com/example/discopedia/discopedia/models/User.java
@@ -0,0 +1,22 @@
+package com.example.discopedia.discopedia.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+
+}
