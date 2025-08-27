68a8524b08a60123b7d54962dd00881b58652139
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-08-27 10:22:26 +0200

    feat(MusicRecord): add JPA annotations and Lombok annotations

--- src/main/java/com/example/discopedia/discopedia/musicrecords/MusicRecord.java
+++ src/main/java/com/example/discopedia/discopedia/musicrecords/MusicRecord.java
@@ -1,4 +1,15 @@
 package com.example.discopedia.discopedia.musicrecords;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "music_records")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MusicRecord {
 }
