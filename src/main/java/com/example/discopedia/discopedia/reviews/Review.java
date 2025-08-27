111b98afba17baa303cbade1432cdc125178202e
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-08-27 11:32:54 +0200

    feat(Review): add JPA annotations and Lombok annotations

--- src/main/java/com/example/discopedia/discopedia/reviews/Review.java
+++ src/main/java/com/example/discopedia/discopedia/reviews/Review.java
@@ -1,4 +1,15 @@
 package com.example.discopedia.discopedia.reviews;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "reviews")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Review {
 }
