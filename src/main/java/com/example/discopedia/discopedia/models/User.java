3333a841cdb67418f6a8a5a80466299b86c18883
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-08-27 09:55:14 +0200

    feat(User): add review field

--- src/main/java/com/example/discopedia/discopedia/models/User.java
+++ src/main/java/com/example/discopedia/discopedia/models/User.java
@@ -28,4 +28,6 @@ public class User {
     @OneToMany(mappedBy = "user", cascade =CascadeType.ALL, orphanRemoval = true)
     private List<Record> records;
 
+    @OneToMany(mappedBy="user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews;
 }
