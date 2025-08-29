2943e512e6ba4679c05b7df8c6a61f8c2e97799d
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-08-29 14:38:17 +0200

    feat(UserRegisterRequest): add password field and validation

--- src/main/java/com/example/discopedia/discopedia/users/dtos/UserRegisterRequest.java
+++ src/main/java/com/example/discopedia/discopedia/users/dtos/UserRegisterRequest.java
@@ -1,8 +1,6 @@
 package com.example.discopedia.discopedia.users.dtos;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record UserRegisterRequest(
         @NotBlank(message="Username is required")
@@ -12,8 +10,10 @@ public record UserRegisterRequest(
         @Email(message="Email should be valid", regexp="regexp = \"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}$")
         @NotBlank(message="Email is required")
         @Size (min=3, max=100, message = "Email must be between 3 and 100 characters")
-        String email
+        String email,
 
-
+        @NotEmpty(message = "Password is required")
+        @Pattern(message = "Password must contain a minimum of 8 characters and a max of 50 characters, including a number, one uppercase letter, one lowercase letter and one special character", regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+=\\-{}\\[\\]:;\"'<>,.?/~])(?=\\S+$).{8,50}$")
+        String password
 ) {
 }
