6bf6e8445a85ea928802db167e4e6e493f66868d
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-08-31 00:16:36 +0200

    build(CustomUserDetail): add CustomUserDetail file

---
+++ src/main/java/com/example/discopedia/discopedia/security/CustomUserDetail.java
@@ -0,0 +1,31 @@
+package com.example.discopedia.discopedia.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetail implements UserDetails {
+    private final User user;
+
+    public CustomUserDetail(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
