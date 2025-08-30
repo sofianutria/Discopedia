53fd26cf30c1f4dbeb7056fcf814bcc858c40d67
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-08-31 00:25:19 +0200

    feat(SecurityConfig): delete cookie for constant login

--- src/main/java/com/example/discopedia/discopedia/security/SecurityConfig.java
+++ src/main/java/com/example/discopedia/discopedia/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,7 +18,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/cd").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(manager->manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 }
