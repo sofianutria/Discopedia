9275f876be2073094cf87433599addd6ba306a0c
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-08-31 09:12:53 +0200

    feat(UserService): add getAllUsers method

--- src/main/java/com/example/discopedia/discopedia/users/UserService.java
+++ src/main/java/com/example/discopedia/discopedia/users/UserService.java
@@ -1,9 +1,22 @@
 package com.example.discopedia.discopedia.users;
 
+import com.example.discopedia.discopedia.users.dtos.UserMapper;
+import com.example.discopedia.discopedia.users.dtos.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public List<UserResponse> getAllUsers(){
+        return userRepository.findAll()
+                .stream()
+                .map(UserMapper::toDto)
+                .toList();
+    }
+
 }
