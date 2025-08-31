9ffb0feff9c4bf2d068516034741c2f18e305679
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-08-31 17:46:14 +0200

    feat(GlobalExceptionHandler): add EntityNotFoundException

--- src/main/java/com/example/discopedia/discopedia/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/discopedia/discopedia/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,17 @@
 package com.example.discopedia.discopedia.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity(ErrorResponse) handleEventNotFoundException(EntityNotFoundException exception, HttpServletRequest req){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorResponse errorResponse = new ErrorResponse(status, exception.getMessage(), req);
+        return new ResponseEntity<>(errorResponse, status);
+    }
 }
