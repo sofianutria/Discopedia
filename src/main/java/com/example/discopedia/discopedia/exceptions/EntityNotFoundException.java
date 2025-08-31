107d88a7a42240303739dee70f784f081634d5f2
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-08-31 18:03:43 +0200

    fix(UserService, EntityNotFoundException): fix message when EntityNotFoundException works in getUserById

--- src/main/java/com/example/discopedia/discopedia/exceptions/EntityNotFoundException.java
+++ src/main/java/com/example/discopedia/discopedia/exceptions/EntityNotFoundException.java
@@ -1,7 +1,7 @@
 package com.example.discopedia.discopedia.exceptions;
 
 public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
+    public EntityNotFoundException(String entityClass, String attributeName, String attributeValue) {
+        super(String.format("%s with %s \"%s\" not found", entityClass, attributeName, attributeValue));
     }
 }
