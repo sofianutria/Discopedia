50e8ebce98267549f54a2b58f1719e5553651c09
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-08-31 09:16:10 +0200

    build(UserController): add UserController class and annotations

---
+++ src/main/java/com/example/discopedia/discopedia/users/UserController.java
@@ -0,0 +1,12 @@
+package com.example.discopedia.discopedia.users;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+}
