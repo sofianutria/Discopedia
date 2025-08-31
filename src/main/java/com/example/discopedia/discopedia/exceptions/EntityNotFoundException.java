976444764aa966e59f7cb378174168086120c478
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-08-31 17:40:22 +0200

    build(EntityNotFoundException): add EntityNotFoundException file

---
+++ src/main/java/com/example/discopedia/discopedia/exceptions/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.discopedia.discopedia.exceptions;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
