e05759257e6a67119a0a8f5c78922e226b49bb3e
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-08-31 17:39:21 +0200

    feat(UserService): add getUserById field

--- src/main/java/com/example/discopedia/discopedia/users/UserService.java
+++ src/main/java/com/example/discopedia/discopedia/users/UserService.java
@@ -2,10 +2,14 @@ package com.example.discopedia.discopedia.users;
 
 import com.example.discopedia.discopedia.users.dtos.UserMapper;
 import com.example.discopedia.discopedia.users.dtos.UserResponse;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,4 +23,10 @@ public class UserService {
                 .toList();
     }
 
+    private UserResponse getUserById(Long id){
+        User user = userRepository.findById(id)
+                .orElseThrow(()->new EntityNotFoundException(User.class.getSimpleName(), "id", id.toString()));
+        return UserMapper.toDto(user);
+
+    }
 }
