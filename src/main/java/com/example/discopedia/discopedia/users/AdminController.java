ff72db6c5aa1c9a0b6675538a2d932faedddff18
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-08-31 09:23:56 +0200

    feat(AdminController): add getAllUsers endpoint

--- src/main/java/com/example/discopedia/discopedia/users/AdminController.java
+++ src/main/java/com/example/discopedia/discopedia/users/AdminController.java
@@ -1,12 +1,23 @@
 package com.example.discopedia.discopedia.users;
 
+import com.example.discopedia.discopedia.users.dtos.UserResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminController {
     private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserResponse>> getAllUsers(){
+        List<UserResponse> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
 }
