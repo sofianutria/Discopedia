b35da538e08462a20dbbba709a33f54c9d78bddd
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-08-31 09:49:04 +0200

    fix(application.yaml): fix db connection configuration

--- src/main/java/com/example/discopedia/discopedia/DiscopediaApplication.java
+++ src/main/java/com/example/discopedia/discopedia/DiscopediaApplication.java
@@ -2,8 +2,6 @@ package com.example.discopedia.discopedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class DiscopediaApplication {
@@ -12,8 +10,4 @@ public class DiscopediaApplication {
 		SpringApplication.run(DiscopediaApplication.class, args);
 	}
 
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder(){
-		return new BCryptPasswordEncoder();
-	}
 }
