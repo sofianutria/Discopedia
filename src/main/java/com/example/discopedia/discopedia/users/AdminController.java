90417153236395427e265d41c0aabc5f0bbb4f9c
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-01 12:51:19 +0200

    feat(AdminController): add deleteUserById endpoint

--- src/main/java/com/example/discopedia/discopedia/users/AdminController.java
+++ src/main/java/com/example/discopedia/discopedia/users/AdminController.java
@@ -3,6 +3,7 @@ package com.example.discopedia.discopedia.users;
 import com.example.discopedia.discopedia.users.dtos.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +25,11 @@ public class AdminController {
     public ResponseEntity<UserResponse> getUserById(Long id){
         UserResponse user = userService.getUserByIdAdmin(id);
         return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<String> deleteUserById(Long id){
+        String message = userService.deleteUserByIdAdmin(id);
+        return ResponseEntity.ok(message);
     }
 }
