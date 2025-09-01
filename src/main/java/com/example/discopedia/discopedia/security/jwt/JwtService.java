f8ac13b6fc0ffd417c2323e4db5cd18cce0bf1a8
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-01 14:04:26 +0200

    feat(JwtService): add buildToken

--- src/main/java/com/example/discopedia/discopedia/security/jwt/JwtService.java
+++ src/main/java/com/example/discopedia/discopedia/security/jwt/JwtService.java
@@ -1,9 +1,12 @@
 package com.example.discopedia.discopedia.security.jwt;
 
+import com.example.discopedia.discopedia.security.CustomUserDetail;
 import com.example.discopedia.discopedia.users.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -13,4 +16,20 @@ public class JwtService {
     private final String ROLE = "role";
     private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
+    
+    public String generateToken(CustomUserDetail userDetail){return buildToken(userDetail, JWT_EXPIRATION);}
+
+    private String buildToken(CustomUserDetail userDetail, Long jwtExpiration) {
+        return Jwts
+                .builder()
+                .claim(ROLE, userDetail.getAuthorities().toString())
+                .subject(userDetail.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+ jwtExpiration))
+                .signWith(getSignKey())
+                .compact();
+    }
+
+    private Object getSignKey() {
+    }
 }
