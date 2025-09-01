82b322434eedccdf152c90f38a7f7a1d7adfc29e
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-01 13:49:31 +0200

    feat(UserRepository): add findByUsername

--- src/main/java/com/example/discopedia/discopedia/users/UserRepository.java
+++ src/main/java/com/example/discopedia/discopedia/users/UserRepository.java
@@ -1,6 +1,8 @@
 package com.example.discopedia.discopedia.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
