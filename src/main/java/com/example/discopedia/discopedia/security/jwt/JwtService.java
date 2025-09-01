cc7b149a380c43c3e56aa919df4cda12e4c19e59
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-01 13:58:34 +0200

    build(JwtService): add JwtService class

---
+++ src/main/java/com/example/discopedia/discopedia/security/jwt/JwtService.java
@@ -0,0 +1,9 @@
+package com.example.discopedia.discopedia.security.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+}
