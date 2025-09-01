06089dc463da7c0e38b594a837877219a8f813b0
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-01 15:00:57 +0200

    feat(AuthController): add parametres

--- src/main/java/com/example/discopedia/discopedia/security/AuthController.java
+++ src/main/java/com/example/discopedia/discopedia/security/AuthController.java
@@ -1,9 +1,18 @@
 package com.example.discopedia.discopedia.security;
 
+import com.example.discopedia.discopedia.security.jwt.JwtService;
+import com.example.discopedia.discopedia.users.UserService;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableMethodSecurity
 @RestController
 public class AuthController {
+    private final UserService userService;
+    private final JwtService jwtService;
+
+    public AuthController(UserService userService, JwtService jwtService) {
+        this.userService = userService;
+        this.jwtService = jwtService;
+    }
 }
