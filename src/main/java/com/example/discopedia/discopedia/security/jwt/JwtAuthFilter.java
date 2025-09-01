223892f0195ce20897f66e295adb914655c07525
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-01 14:41:26 +0200

    feat(JwtAuthFilter): add JwtAuthFilter for JWT authentication

--- src/main/java/com/example/discopedia/discopedia/security/jwt/JwtAuthFilter.java
+++ src/main/java/com/example/discopedia/discopedia/security/jwt/JwtAuthFilter.java
@@ -1,4 +1,25 @@
 package com.example.discopedia.discopedia.security.jwt;
 
-public class JwtAuthFilter {
+import com.example.discopedia.discopedia.users.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserService userService;
+
+    public JwtAuthFilter(JwtService jwtService, UserService userService){
+        this.jwtService = jwtService;
+        this.userService = userService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
