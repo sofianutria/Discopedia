ec16373b30ca81a782f886013cf93d8575eea8b3
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-01 13:56:42 +0200

    build(AuthController): add AuthController class

---
+++ src/main/java/com/example/discopedia/discopedia/security/AuthController.java
@@ -0,0 +1,9 @@
+package com.example.discopedia.discopedia.security;
+
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableMethodSecurity
+@RestController
+public class AuthController {
+}
