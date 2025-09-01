ea4bd95b6337ba420ba6780e5f1ba57dcb90d3ba
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-01 14:00:51 +0200

    feat(JwtService): add config constants

--- src/main/java/com/example/discopedia/discopedia/security/jwt/JwtService.java
+++ src/main/java/com/example/discopedia/discopedia/security/jwt/JwtService.java
@@ -1,9 +1,16 @@
 package com.example.discopedia.discopedia.security.jwt;
 
+import com.example.discopedia.discopedia.users.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class JwtService {
+    private final String JWT_SECRET_KEY="mySecretKeyForJWTTokenGenerationThatIsAtLeast256BitsLong";
+    private final Long JWT_EXPIRATION=1800000L;
+    private final String ROLE = "role";
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
 }
