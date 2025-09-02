d8cf1093a45dd543c4b5b8bdae8aeef40f58bc02
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-02 11:16:14 +0200

    feat(AdminController): add authentication into endpoints

--- src/main/java/com/example/discopedia/discopedia/users/AdminController.java
+++ src/main/java/com/example/discopedia/discopedia/users/AdminController.java
@@ -1,14 +1,14 @@
 package com.example.discopedia.discopedia.users;
 
 import com.example.discopedia.discopedia.common.SecuredBaseController;
+import com.example.discopedia.discopedia.security.CustomUserDetail;
 import com.example.discopedia.discopedia.users.dtos.UserResponse;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,18 +20,26 @@ public class AdminController extends SecuredBaseController {
     private final UserService userService;
 
     @GetMapping("/users")
-    public ResponseEntity<List<UserResponse>> getAllUsers(){
+    public ResponseEntity<List<UserResponse>> getAllUsers(
+            @AuthenticationPrincipal CustomUserDetail userDetail
+            ){
         List<UserResponse> users = userService.getAllUsers();
         return ResponseEntity.ok(users);
     }
     @GetMapping("/users/{id}")
-    public ResponseEntity<UserResponse> getUserById(Long id){
+    public ResponseEntity<UserResponse> getUserById(
+            @AuthenticationPrincipal CustomUserDetail userDetail,
+            @PathVariable @Positive(message="User id must be a positive number")
+            Long id){
         UserResponse user = userService.getUserByIdAdmin(id);
         return ResponseEntity.ok(user);
     }
 
     @DeleteMapping("/users/{id}")
-    public ResponseEntity<String> deleteUserById(Long id){
+    public ResponseEntity<String> deleteUserById(
+            @AuthenticationPrincipal CustomUserDetail userDetail,
+            @PathVariable @Positive (message="User id must be a positive number")
+            Long id){
         String message = userService.deleteUserByIdAdmin(id);
         return ResponseEntity.ok(message);
     }
