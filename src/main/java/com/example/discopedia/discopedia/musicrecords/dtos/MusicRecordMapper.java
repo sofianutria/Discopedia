4d12b0d22062288e564c2cd38f8bbce39c96b181
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-02 15:04:11 +0200

    feat(MusicRecordMapper): add toEntity in MusicRecordMapper

--- src/main/java/com/example/discopedia/discopedia/musicrecords/dtos/MusicRecordMapper.java
+++ src/main/java/com/example/discopedia/discopedia/musicrecords/dtos/MusicRecordMapper.java
@@ -1,4 +1,17 @@
 package com.example.discopedia.discopedia.musicrecords.dtos;
 
+import com.example.discopedia.discopedia.musicrecords.MusicRecord;
+import com.example.discopedia.discopedia.users.User;
+
 public class MusicRecordMapper {
+    public static MusicRecord toEntity(MusicRecordRequest dto, User user){
+        return MusicRecord.builder()
+                .title(dto.title())
+                .artist(dto.artist())
+                .musicalGenre(dto.musicalGenre())
+                .year(dto.year())
+                .setlist(dto.setlist())
+                .imageUrl(dto.image())
+                .build();
+    }
 }
