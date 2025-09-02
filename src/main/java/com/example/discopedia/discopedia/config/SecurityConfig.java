c695497d0fd4390cf42b541efcc5aa2b3c642f1d
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-02 12:20:08 +0200

    feat(SecurityConfig): add authenticationEntryPoint

--- src/main/java/com/example/discopedia/discopedia/config/SecurityConfig.java
+++ src/main/java/com/example/discopedia/discopedia/config/SecurityConfig.java
@@ -1,20 +1,25 @@
 package com.example.discopedia.discopedia.config;
 
+import com.example.discopedia.discopedia.exceptions.ErrorResponse;
 import com.example.discopedia.discopedia.security.jwt.JwtAuthFilter;
 import com.example.discopedia.discopedia.security.jwt.JwtService;
 import com.example.discopedia.discopedia.users.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -36,6 +41,17 @@ public class SecurityConfig {
 
     @Bean
     public JwtAuthFilter jwtAuthFilter (JwtService jwtService){return new JwtAuthFilter(jwtService, userService)}
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint(){
+        return (request, response, exception) ->{
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            ErrorResponse error = new ErrorResponse(
+                    HttpStatus.UNAUTHORIZED, "Unauthorized: " + exception.getMessage(), request);
+            objectMapper().writeValue(response.getWriter(),error);
+        };
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
