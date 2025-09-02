cb7e4a51a3c778f0d7ec8d3f549db0cf2cb95f92
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-02 10:15:26 +0200

    feat(UserController): add getMyUser endpoint

--- src/main/java/com/example/discopedia/discopedia/users/UserController.java
+++ src/main/java/com/example/discopedia/discopedia/users/UserController.java
@@ -1,6 +1,12 @@
 package com.example.discopedia.discopedia.users;
 
+import com.example.discopedia.discopedia.musicrecords.MusicRecordService;
+import com.example.discopedia.discopedia.security.CustomUserDetail;
+import com.example.discopedia.discopedia.users.dtos.UserResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final MusicRecordService musicRecordService;
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponse> getMyUser(
+            @AuthenticationPrincipal CustomUserDetail userDetail){
+        UserResponse user = userService.getOwnUser(userDetail.getId());
+        return ResponseEntity.ok(user);
+    }
 }
