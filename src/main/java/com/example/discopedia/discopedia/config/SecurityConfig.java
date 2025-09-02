5665403239422e24a953b6a3e713532d01a3714f
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-02 11:28:04 +0200

    refactor(SecurityConfig): add SecurityConfig to new package config

--- src/main/java/com/example/discopedia/discopedia/config/SecurityConfig.java
+++ src/main/java/com/example/discopedia/discopedia/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.discopedia.discopedia.security;
+package com.example.discopedia.discopedia.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
