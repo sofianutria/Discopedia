5591e8207121ea3c5adb705416628076a520b263
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-02 15:09:26 +0200

    feat(MusicRecordMapper): add toDto in MusicRecordMapper

--- src/main/java/com/example/discopedia/discopedia/musicrecords/dtos/MusicRecordMapper.java
+++ src/main/java/com/example/discopedia/discopedia/musicrecords/dtos/MusicRecordMapper.java
@@ -2,6 +2,8 @@ package com.example.discopedia.discopedia.musicrecords.dtos;
 
 import com.example.discopedia.discopedia.musicrecords.MusicRecord;
 import com.example.discopedia.discopedia.users.User;
+import com.example.discopedia.discopedia.users.dtos.UserMapper;
+import com.example.discopedia.discopedia.users.dtos.UserResponse;
 
 public class MusicRecordMapper {
     public static MusicRecord toEntity(MusicRecordRequest dto, User user){
@@ -13,5 +15,9 @@ public class MusicRecordMapper {
                 .setlist(dto.setlist())
                 .imageUrl(dto.image())
                 .build();
+    }
+    public static MusicRecordResponse toDto(MusicRecord musicRecord){
+        UserResponse userDto = UserMapper.toDto(musicRecord.getUser());
+        return new MusicRecordResponse(musicRecord.getId(), musicRecord.getTitle(), musicRecord.getArtist(), musicRecord.getMusicalGenre(), musicRecord.getYear(), musicRecord.getSetlist(), musicRecord.getImageUrl(), userDto);
     }
 }
