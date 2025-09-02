7538dc8db50219b084d97007799818ed54ecf307
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-02 14:58:24 +0200

    feat(MusicRecordRequest): add artist field

--- src/main/java/com/example/discopedia/discopedia/musicrecords/dtos/MusicRecordRequest.java
+++ src/main/java/com/example/discopedia/discopedia/musicrecords/dtos/MusicRecordRequest.java
@@ -4,6 +4,9 @@ import jakarta.validation.constraints.NotBlank;
 
 public record MusicRecordRequest(
         @NotBlank(message= "Title is required")
-        String title
+        String title,
+
+        @NotBlank(message= "Artist is required")
+        String artist
 ) {
 }
