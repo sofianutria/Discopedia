52c233bbb00f6b6dd1d0dc47761686d239826031
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-02 12:38:33 +0200

    feat(SecurityConfig): add authenticationManager

--- src/main/java/com/example/discopedia/discopedia/config/SecurityConfig.java
+++ src/main/java/com/example/discopedia/discopedia/config/SecurityConfig.java
@@ -14,7 +14,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -78,5 +80,10 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
     }
 }
