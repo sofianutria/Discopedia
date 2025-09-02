b1dac966cbd5b65d7441507a594eae021b924f9b
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-02 14:55:17 +0200

    feat(MusicRecordResponse): add fields

--- src/main/java/com/example/discopedia/discopedia/musicrecords/dtos/MusicRecordResponse.java
+++ src/main/java/com/example/discopedia/discopedia/musicrecords/dtos/MusicRecordResponse.java
@@ -1,4 +1,13 @@
 package com.example.discopedia.discopedia.musicrecords.dtos;
 
-public record MusicRecordResponse() {
+public record MusicRecordResponse(
+        Long id,
+        String title,
+        String artist,
+        String musicalGenre,
+        Integer year,
+        String setlist,
+        String imageUrl,
+        String user
+) {
 }
