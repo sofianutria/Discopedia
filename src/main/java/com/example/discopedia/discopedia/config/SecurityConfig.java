c126a920ea954ecf0c2be97195437891465ab316
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-02 12:08:59 +0200

    feat(SecurityConfig): add db config

--- src/main/java/com/example/discopedia/discopedia/config/SecurityConfig.java
+++ src/main/java/com/example/discopedia/discopedia/config/SecurityConfig.java
@@ -1,15 +1,25 @@
 package com.example.discopedia.discopedia.config;
 
+import com.example.discopedia.discopedia.users.UserService;
+import lombok.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+    private final UserService userService;
+    @Value ("${port.front}")
+    private String frontPort;
+    public SecurityConfig(UserService userService){this.userService=userService;}
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
