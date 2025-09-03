2316c6c0790a5f1bc359cbd975981b3b6093ee79
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-03 12:22:06 +0200

    feat(MusicRecordService): add getAllDestinations and listToDtoShort

--- src/main/java/com/example/discopedia/discopedia/musicrecords/MusicRecordService.java
+++ src/main/java/com/example/discopedia/discopedia/musicrecords/MusicRecordService.java
@@ -2,6 +2,7 @@ package com.example.discopedia.discopedia.musicrecords;
 
 import com.example.discopedia.discopedia.musicrecords.dtos.MusicRecordResponse;
 import com.example.discopedia.discopedia.musicrecords.dtos.MusicRecordMapper;
+import com.example.discopedia.discopedia.musicrecords.dtos.MusicRecordResponseShort;
 import com.example.discopedia.discopedia.users.User;
 import com.example.discopedia.discopedia.users.UserService;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,17 @@ import java.util.List;
 public class MusicRecordService {
     private final MusicRecordRepository musicRecordRepository;
     private final UserService userService;
+
+    public List<MusicRecordResponseShort> getAllDestinations(){
+        List<MusicRecord> musicRecords = musicRecordRepository.findAll();
+        return listToDtoShort(musicRecords);
+    }
+
+    private List<MusicRecordResponseShort> listToDtoShort(List<MusicRecord> musicRecords) {
+        return musicRecords.stream()
+                .map(MusicRecordMapper::toDtoShort)
+                .toList();
+    }
 
     public List<MusicRecordResponse> getCdsByUserUsername(String username) {
         User user = userService.getByUsername(username);
