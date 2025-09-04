ff131bb2b7be52e8a87ac9ee326da9c0a66cc0a1
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-04 13:50:40 +0200

    feat(ReviewRepository): add JpaRepository

--- src/main/java/com/example/discopedia/discopedia/reviews/ReviewRepository.java
+++ src/main/java/com/example/discopedia/discopedia/reviews/ReviewRepository.java
@@ -1,4 +1,8 @@
 package com.example.discopedia.discopedia.reviews;
 
-public interface ReviewRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
