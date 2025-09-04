0b2a11de71214c93c3505dca0b242f74977bb224
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-04 14:36:14 +0200

    feat(ReviewService, ReviewRepository): add addReview field

--- src/main/java/com/example/discopedia/discopedia/reviews/ReviewRepository.java
+++ src/main/java/com/example/discopedia/discopedia/reviews/ReviewRepository.java
@@ -1,8 +1,11 @@
 package com.example.discopedia.discopedia.reviews;
 
+import com.example.discopedia.discopedia.musicrecords.MusicRecord;
+import com.example.discopedia.discopedia.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    boolean existsByUserAndMusicRecord(User user, MusicRecord musicRecord);
 }
--- src/main/java/com/example/discopedia/discopedia/reviews/ReviewService.java
+++ src/main/java/com/example/discopedia/discopedia/reviews/ReviewService.java
@@ -1,7 +1,15 @@
 package com.example.discopedia.discopedia.reviews;
 
+import com.example.discopedia.discopedia.exceptions.EntityAlreadyExistsException;
+import com.example.discopedia.discopedia.exceptions.EntityNotFoundException;
+import com.example.discopedia.discopedia.musicrecords.MusicRecord;
 import com.example.discopedia.discopedia.musicrecords.MusicRecordRepository;
+import com.example.discopedia.discopedia.reviews.dtos.ReviewMapper;
+import com.example.discopedia.discopedia.reviews.dtos.ReviewRequest;
+import com.example.discopedia.discopedia.reviews.dtos.ReviewResponse;
+import com.example.discopedia.discopedia.users.User;
 import com.example.discopedia.discopedia.users.UserRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,5 +22,20 @@ public class ReviewService {
         this.reviewRepository = reviewRepository;
         this.userRepository = userRepository;
         this.musicRecordRepository = musicRecordRepository;
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    public ReviewResponse addReview(Long userId, ReviewRequest request) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("User", "id", userId.toString()));
+        MusicRecord musicRecord = musicRecordRepository.findById(request.musicRecordId())
+                .orElseThrow(() -> new EntityNotFoundException("Music record", "id", request.musicRecordId().toString()));
+        boolean exists = reviewRepository.existsByUserAndMusicRecord(user, musicRecord);
+        if (exists) {
+            throw new EntityAlreadyExistsException("Review", "userId + musicRecordId", user.getId() + " & " + musicRecord.getId());
+        }
+        Review review = ReviewMapper.toEntity(request, user, musicRecord);
+        reviewRepository.save(review);
+        return ReviewMapper.toDto(review);
     }
 }
