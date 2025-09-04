e601a6f65d1fcf9dc4d2bcc1e487cbfce0677b28
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-04 14:22:32 +0200

    fix(ReviewResponse): fix change UserResponse to UserResponseShort

--- src/main/java/com/example/discopedia/discopedia/reviews/dtos/ReviewResponse.java
+++ src/main/java/com/example/discopedia/discopedia/reviews/dtos/ReviewResponse.java
@@ -1,13 +1,13 @@
 package com.example.discopedia.discopedia.reviews.dtos;
 
 import com.example.discopedia.discopedia.musicrecords.dtos.MusicRecordResponseShort;
-import com.example.discopedia.discopedia.users.dtos.UserResponse;
+import com.example.discopedia.discopedia.users.dtos.UserResponseShort;
 
 public record ReviewResponse(
         Long id,
         int qualification,
         String reviewDescription,
-        UserResponse username,
+        UserResponseShort username,
         MusicRecordResponseShort musicRecordResponseShort
 ) {
 }
