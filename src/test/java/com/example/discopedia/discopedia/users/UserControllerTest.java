276eee18a639d01e8f955369cadbd8bc214d9437
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-04 19:18:49 +0200

    feat(UserControllerTest): add getMyUser_whenAuthenticated_returnsUserResponse

--- src/test/java/com/example/discopedia/discopedia/users/UserControllerTest.java
+++ src/test/java/com/example/discopedia/discopedia/users/UserControllerTest.java
@@ -8,19 +8,23 @@ import com.example.discopedia.discopedia.security.CustomUserDetail;
 import com.example.discopedia.discopedia.users.dtos.UserRegisterRequest;
 import com.example.discopedia.discopedia.users.dtos.UserResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
 import java.util.ArrayList;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -56,5 +60,35 @@ public class UserControllerTest {
                 .webAppContextSetup(context)
                 .apply(springSecurity())
                 .build();
+    }
+    @AfterEach
+    void afterTest(){
+        verifyNoMoreInteractions(userService);
+    }
+    private String asJsonString(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (Exception exception) {
+            throw new RuntimeException("Failed to convert object to JSON string for testing", exception);
+        }
+    }
+
+    @Nested
+    @DisplayName("GET /users")
+    class GetUsersTests{
+        @Test
+        void getMyUser_whenAuthenticated_returnsUserResponse() throws Exception {
+            when(userService.getOwnUser(customUserDetail.getId())).thenReturn(userResponse);
+            mockMvc.perform(get("/users/me")
+                            .principal(() -> customUserDetail.getUsername()))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                    .andExpect(jsonPath("$.id").value(userResponse.id()))
+                    .andExpect(jsonPath("$.username").value(userResponse.username()))
+                    .andExpect(jsonPath("$.email").value(userResponse.email()))
+                    .andExpect(jsonPath("$.role").value(userResponse.role()));
+            verify(userService, times(1)).getOwnUser(customUserDetail.getId());
+        }
+
     }
 }
